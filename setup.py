ed38210c2234634b40ed8ac1ac198d37ce71d48a
Konstantin Koslowski <konstantin.koslowski@gmail.com>
2017-12-18 21:33:28 +0100

    snapcastrd: add executable

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
   name='snapcastr',
   packages=['snapcastr'],
+  scripts=['bin/snapcastrd'],
   include_package_data=True,
   install_requires=[ 'flask', 'flask_bootstrap', 'flask_nav', 'flask-wtf', 'snapcast', 'wtforms']
 )
