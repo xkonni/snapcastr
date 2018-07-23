8ca1e7426ebe27f397388890ee3daad05a9520a9
User65k <d4n@posteo.de>
2018-07-23 19:51:29 +0200

    don't get zipped (it breaks static files in flask?)

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ setup(
   packages=['snapcastr'],
   scripts=['bin/snapcastrd'],
   include_package_data=True,
+  zip_safe=False,
   install_requires=[ 'flask', 'flask_bootstrap', 'flask_nav', 'snapcast', 'wtforms']
 )
